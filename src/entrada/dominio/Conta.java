5a245917c50d8ef448ea293003d80e880c907288
Edgar Oliveira Souza <edgaroliveirasouza1@gmail.com>
2026-05-28 19:08:59 -0300

    Feito absolutamente todo o sistema de login, mas de olho no bug do Scanner

--- src/entrada/dominio/Conta.java
+++ src/entrada/dominio/Conta.java
@@ -12,7 +12,28 @@ public class Conta {
         System.out.println("Crie a sua conta");
         System.out.print("Seu Nome de usuario(a): ");
         String nome = scanner.nextLine();
-        System.out.println("Seu Email: ");
+        this.nome = nome;
+        System.out.print("Seu Email: ");
         String email = scanner.nextLine();
+        this.email = email;
+        System.out.print("Nova Senha: ");
+        String senha = scanner.nextLine();
+        this.senha = senha;
+    }
+
+    public void Logar(){
+        do{
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+            System.out.print("Senha: ");
+            String senha = scanner.nextLine();
+            if(nome.equals(this.nome) && senha.equals(this.senha)){
+                System.out.println("Bem vindo a sua conta");
+                System.out.println("Login feito com sucesso!");
+                break;
+            }else {
+                System.out.println("Login invalido");
+            }
+        }while(true);
     }
 }
--- src/entrada/entrar/Login.java
+++ src/entrada/entrar/Login.java
@@ -1,7 +1,12 @@
 package entrada.entrar;
 
+import entrada.dominio.Conta;
+
 public class Login {
     public static void main(String[] args){
-
+        Conta henry = new Conta();
+        henry.setAccount();
+        System.out.println();
+        henry.Logar();
     }
 }
